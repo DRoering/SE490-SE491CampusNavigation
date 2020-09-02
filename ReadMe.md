57fd33c6129217f85a6e86fee55e3090db713528
alexwisuri <alexwisuri@gmail.com>
2020-09-02 16:52:07 -0500

    Added ReadMe.md

---
+++ ReadMe.md
@@ -0,0 +1,20 @@
+CampusNavigation
+
+Required steps to work with the project:
+
+1: Install VSCode, NodeJS/npm & ionic CLI tools:
+  --VSCode: https://code.visualstudio.com/Download
+  --NodeJS/npm:  https://nodejs.org/en/
+  --Ionic: run the following using CMD: `npm install -g @ionic/cli`
+
+2: Install the required project dependencies:
+  --Open CMD to your working copy of the repository, then run the following: `npm i`
+
+3: Serve the project using: `ionic serve`
+
+4: Run tests using: `npm run test`
+
+5: Configure VSCode using the following:
+  --Add the `dbaeumer.vscode-eslint` extension
+  --Set the VSCode `editor.tabSize` preference to 2
+  --Enable the VSCode `eslint.format.enable` preference if it is not already
--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import './ExploreContainer.css';
 
-interface ContainerProps { }
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface ContainerProps {}
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <div className="container">
       <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <p>
+        Start with Ionic{' '}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://ionicframework.com/docs/components"
+        >
+          UI Components
+        </a>
+      </p>
     </div>
   );
 };

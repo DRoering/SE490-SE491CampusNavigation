6660ff9f675207a9ac95ffc58e1f6e36c0d9fe14
Alex Wisuri <amwisuri@go.stcloudstate.edu>
2020-10-25 17:52:02 -0500

    Fixed building pins not opening

--- src/components/CampusMap/CampusMap.tsx
+++ src/components/CampusMap/CampusMap.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Map, TileLayer, Marker } from "react-leaflet";
+import { Map, TileLayer } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import "./CampusMap.scss";
@@ -17,10 +17,6 @@ export const CampusMap: React.FC<CampusMapProps> = (props: CampusMapProps) => {
   const position = L.latLng([45.5511, -94.1515]);
   const minimumZoom = 8;
   const zoom = 16;
-  const buildingIcon = L.icon({
-    iconUrl: "assets/mapIcons/businessIcon.png",
-    iconSize: [25, 25],
-  });
 
   useEffect(() => {
     console.debug("resetSize Called");
@@ -42,13 +38,6 @@ export const CampusMap: React.FC<CampusMapProps> = (props: CampusMapProps) => {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
       <BuildingPin buildings={props.buildings} showName={props.showName} />
-      {props.buildings.map((building) => (
-        <Marker
-          key={building.id}
-          position={building.coordinates}
-          icon={buildingIcon}
-        ></Marker>
-      ))}
       <ParkingLotPin parkingLots={props.parkingLots} />
       <EventPin events={props.events}></EventPin>
       ))

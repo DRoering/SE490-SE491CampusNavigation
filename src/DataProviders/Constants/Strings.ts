bb42ee4063bfd3f368e5b2f072d44a114fc1a503
Alex Wisuri <amwisuri@go.stcloudstate.edu>
2020-11-07 20:35:46 -0600

    Constants and missing index from HTTPProvider

---
+++ src/DataProviders/Constants/Strings.ts
@@ -0,0 +1,4 @@
+export const Strings = {
+  apiUrl: "https://api.airtable.com/v0/appX1ffTov7j8hqP4/",
+  apiKey: "",
+};
--- src/DataProviders/index.ts
+++ src/DataProviders/index.ts
@@ -1,3 +1,4 @@
+export * from "./Constants";
 export * from "./useEvent";
 export * from "./useBuilding";
 export * from "./useParkingLot";

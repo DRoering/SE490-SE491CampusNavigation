802a5dbfde824d528333c74f73c78c4fff394fd3
Alex Wisuri <amwisuri@go.stcloudstate.edu>
2020-11-20 14:05:40 -0600

    Clicking on SCSU logo now links to scsu website

--- src/components/HeaderBar/HeaderBar.tsx
+++ src/components/HeaderBar/HeaderBar.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { IonHeader, IonToolbar } from "@ionic/react";
+import { Strings } from "../../DataProviders";
 import "./HeaderBar.scss";
 
 export const HeaderBar: React.FC = () => (
   <IonHeader>
     <IonToolbar color="primary">
-      <img
-        id="scsu-logo"
-        src="assets/images/scsu_logo.png"
-        alt="SCSU Logo"
-        slot="start"
-      />
+      <a href={Strings.scsuUrl} target="_blank" rel="noopener noreferrer">
+        <img
+          id="scsu-logo"
+          src="assets/images/scsu_logo.png"
+          alt="SCSU Logo"
+          slot="start"
+        />
+      </a>
     </IonToolbar>
   </IonHeader>
 );

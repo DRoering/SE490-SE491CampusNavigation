5305fa000d645b051482f67d82cbc19874dfe8cd
JRice45 <sq9871sq@go.minnstate.edu>
2021-01-22 16:46:02 -0600

    Added alphabetical sorting function, is now usable through SortMenuComponents

---
+++ src/components/SortMenuComponents/SortAlpha/SortAlpha.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Moment from "moment";
+import { CommonProperties } from "../../../Reuseable";
+
+interface ArrayListProps<T> extends CommonProperties {
+  tabArray: Array<T>[];
+  openModal: (y: Array<T>) => void;
+}
+
+export const sortArrayAlpha = (y: ArrayListProps<>[]) => {
+  y.sort((a, b) => {
+    if (a.name.toLowerCase() > b.name.toLowerCase()) {
+      return 1;
+    }
+    if (a.name.toLowerCase() < b.name.toLowerCase()) {
+      return -1;
+    }
+
+    return 0;
+  });
+  return y;
+};

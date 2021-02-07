36ad1056e7fc00b43ed2b9660c3a5a1d4ba2e473
Alex Wisuri <amwisuri@go.stcloudstate.edu>
2021-02-07 15:51:46 -0600

    Implemented sort by distance with mock location

--- src/DataProviders/SortProvider/SortAlgorithms.ts
+++ src/DataProviders/SortProvider/SortAlgorithms.ts
@@ -1,3 +1,4 @@
+import L from "leaflet";
 import { Building, CampusEvent, Lot, Organization } from "..";
 
 export interface SortType {
@@ -27,8 +28,19 @@ export const SortAlgorithms = {
       a: Building | CampusEvent | Lot | Organization,
       b: Building | CampusEvent | Lot | Organization
     ): number => {
-      //if (a.closerThan(b.coordinates)) return 1;
-      //if (b.closerThan(a.coordinates)) return -1;
+      const fakeLocation = L.latLng([45.551613, -94.148977]);
+      const aDistance = a.coordinates
+        ? a.coordinates.distanceTo(fakeLocation)
+        : 10000;
+      const bDistance = b.coordinates
+        ? b.coordinates.distanceTo(fakeLocation)
+        : 10000;
+
+      console.log("aDistance: " + aDistance);
+      console.log("bDistance: " + bDistance);
+
+      if (aDistance > bDistance) return 1;
+      if (bDistance > aDistance) return -1;
       return 0;
     },
   },
--- src/Reuseable/GeneralDataTypes/CommonProperties.ts
+++ src/Reuseable/GeneralDataTypes/CommonProperties.ts
@@ -14,5 +14,4 @@ export interface CommonProperties {
   virtual?: boolean;
   isOpen?: boolean;
   imgUrl?: string;
-  closerThan: (user: L.LatLng) => boolean;
 }

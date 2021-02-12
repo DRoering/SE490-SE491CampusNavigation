539d23d0f5c991da4689d9d5f8d16f5a1e4551b6
Alex Wisuri <amwisuri@go.stcloudstate.edu>
2021-02-12 15:42:56 -0600

    Added emailjs package and email handler

--- src/DataProviders/Constants/Strings.ts
+++ src/DataProviders/Constants/Strings.ts
@@ -2,6 +2,9 @@ export const Strings = {
   apiUrl: "https://api.airtable.com/v0/appX1ffTov7j8hqP4/",
   apiKey: "keyMSO7kBfGHYMyXm",
   scsuUrl: "https://www.stcloudstate.edu",
+  eventUrl: "huskiesconnect.stcloudstate.edu/images/",
+  feedbackId: "template_ig98ogk",
+  serviceId: "service_1gkznkd",
 };
 
 const baseOptions = ["Alpha", "Distance"];

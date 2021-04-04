2705c544cb97dd4c52731251184ab8cfeccb18a8
Alex Wisuri <ug6709ql@go.minnstate.edu>
2021-04-04 17:02:28 -0500

    fixed padding, menu and scsu icons now hide themselves

--- src/components/HeaderBar/HeaderBar.tsx
+++ src/components/HeaderBar/HeaderBar.tsx
@@ -18,28 +18,34 @@ interface HeaderBarProps {
 
 export const HeaderBar: React.FC<HeaderBarProps> = (props: HeaderBarProps) => {
   const [searchText, setSearchText] = useState("");
+  const [hideIcons, setHideIcons] = useState(false);
 
   return (
     <IonHeader>
-      <IonToolbar color="primary">
-        <a
-          slot="start"
-          href={Strings.scsuUrl}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            id="scsu-logo"
-            src="assets/images/scsu_logo.png"
-            alt="SCSU Logo"
+      <IonToolbar id="header-bar" color="primary">
+        {!hideIcons && (
+          <a
             slot="start"
-          />
-        </a>
+            href={Strings.scsuUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              id="scsu-logo"
+              src="assets/images/scsu_logo.png"
+              alt="SCSU Logo"
+              slot="start"
+            />
+          </a>
+        )}
         <IonSearchbar
+          id="search-bar"
+          onIonFocus={() => setHideIcons(true)}
+          onIonBlur={() => setHideIcons(false)}
           value={searchText}
           // onIonChange={(e) => setSearchText(e.detail.value!)}
         />
-        {props.displayButton && (
+        {props.displayButton && !hideIcons && (
           <div id="options-menu" slot="end">
             <IonButtons>
               <IonMenuButton>

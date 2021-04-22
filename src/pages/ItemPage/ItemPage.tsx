51678ae63948913037e3962a6564e14b2de46d43
Alex Wisuri <ug6709ql@go.minnstate.edu>
2021-04-22 00:21:21 -0500

    Map pins are now searchable & added ability to search for lot type

--- src/pages/ItemPage/ItemPage.tsx
+++ src/pages/ItemPage/ItemPage.tsx
@@ -39,6 +39,7 @@ interface ItemPageProps {
 
 const itemOptions = ["Buildings", "Events", "Parking", "Organizations"];
 const sortOptions = ItemSortOptions.buildingOptions;
+const { searchItems } = Search;
 
 export const ItemPage: React.FC<ItemPageProps> = (props: ItemPageProps) => {
   const [currentItem, setCurrentItem] = useState("Buildings");
@@ -62,7 +63,6 @@ export const ItemPage: React.FC<ItemPageProps> = (props: ItemPageProps) => {
     setShowModal(true);
   };
 
-  const { searchItems } = Search;
   const updateItem = (i: string) => {
     setCurrentItem(i);
   };

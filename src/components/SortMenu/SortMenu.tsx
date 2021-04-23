cc0ad5e5629ae46fe1f6ac0617765c00c40b9f65
Alex Wisuri <ug6709ql@go.minnstate.edu>
2021-04-23 00:12:33 -0500

    Added clear filter button to item page

--- src/components/SortMenu/SortMenu.tsx
+++ src/components/SortMenu/SortMenu.tsx
@@ -19,100 +19,95 @@ interface SortMenuProps {
   sortOptions: string[];
   currentSort: string;
   updateSort: (u?: string) => void;
-  filterByOpen?: (a: boolean) => void;
-  filterByCategory?: (c: string[]) => void;
-  filterByLot?: (c: string) => void;
-  filterByExpired?: (e: boolean) => void;
+  filterByOpen?: { filter: boolean; setFilter: (a: boolean) => void };
+  filterByCategory?: { filter: string[]; setFilter: (c: string[]) => void };
+  filterByLot?: { filter: string; setFilter: (c: string) => void };
+  filterByExpired?: { filter: boolean; setFilter: (e: boolean) => void };
 }
 
-export const SortMenu: React.FC<SortMenuProps> = (props: SortMenuProps) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isExpired, setIsExpired] = useState(false);
-  return (
-    <>
-      <IonMenu side="start" class="sort-menu" contentId="options-menu">
-        <IonContent id="content">
-          <HeaderBar displayButton={false} displaySearch={false} />
-          <IonList>
-            <IonRadioGroup
-              value={props.currentSort}
-              onIonChange={(e) => props.updateSort(e.detail.value)}
-            >
-              {props.sortOptions.map((option) => (
-                <IonItem key={option}>
-                  <IonLabel>{option}</IonLabel>
-                  <IonRadio slot="end" value={option} />
-                </IonItem>
-              ))}
-            </IonRadioGroup>
-            {props.filterByOpen && (
-              <IonItem>
-                <IonLabel>Open</IonLabel>
-                <IonToggle
-                  checked={isOpen}
-                  value="open"
-                  onIonChange={(e) => {
-                    setIsOpen(e.detail.checked);
-                    props.filterByOpen && props.filterByOpen(e.detail.checked);
-                  }}
-                />
+export const SortMenu: React.FC<SortMenuProps> = (props: SortMenuProps) => (
+  <>
+    <IonMenu side="start" class="sort-menu" contentId="options-menu">
+      <IonContent id="content">
+        <HeaderBar displayButton={false} displaySearch={false} />
+        <IonList>
+          <IonRadioGroup
+            value={props.currentSort}
+            onIonChange={(e) => props.updateSort(e.detail.value)}
+          >
+            {props.sortOptions.map((option) => (
+              <IonItem key={option}>
+                <IonLabel>{option}</IonLabel>
+                <IonRadio slot="end" value={option} />
               </IonItem>
-            )}
-            {props.filterByCategory && (
-              <IonItem>
-                <IonLabel>Category</IonLabel>
-                <IonSelect
-                  mode="md"
-                  multiple={true}
-                  cancelText="Cancel"
-                  okText="Confirm"
-                  onIonChange={(e) =>
-                    props.filterByCategory &&
-                    props.filterByCategory(e.detail.value)
-                  }
-                >
-                  {ItemFilterOptions.orgCategories.map((category) => (
-                    <IonSelectOption key={category} value={category}>
-                      {category}
-                    </IonSelectOption>
-                  ))}
-                </IonSelect>
-              </IonItem>
-            )}
-            {props.filterByLot && (
-              <IonRadioGroup
-                allowEmptySelection={true}
+            ))}
+          </IonRadioGroup>
+          {props.filterByOpen && (
+            <IonItem>
+              <IonLabel>Open</IonLabel>
+              <IonToggle
+                checked={props.filterByOpen.filter}
+                value="open"
+                onIonChange={(e) => {
+                  props.filterByOpen &&
+                    props.filterByOpen.setFilter(e.detail.checked);
+                }}
+              />
+            </IonItem>
+          )}
+          {props.filterByCategory && (
+            <IonItem>
+              <IonLabel>Category</IonLabel>
+              <IonSelect
+                mode="md"
+                multiple={true}
+                cancelText="Cancel"
+                okText="Confirm"
                 onIonChange={(e) =>
-                  props.filterByLot && props.filterByLot(e.detail.value)
+                  props.filterByCategory &&
+                  props.filterByCategory.setFilter(e.detail.value)
                 }
               >
-                <IonItem>
-                  <IonLabel>Student</IonLabel>
-                  <IonRadio value="Student" />
-                </IonItem>
-                <IonItem>
-                  <IonLabel>Employee</IonLabel>
-                  <IonRadio value="Employee" />
-                </IonItem>
-              </IonRadioGroup>
-            )}
-            {props.filterByExpired && (
+                {ItemFilterOptions.orgCategories.map((category) => (
+                  <IonSelectOption key={category} value={category}>
+                    {category}
+                  </IonSelectOption>
+                ))}
+              </IonSelect>
+            </IonItem>
+          )}
+          {props.filterByLot && (
+            <IonRadioGroup
+              allowEmptySelection={true}
+              onIonChange={(e) =>
+                props.filterByLot && props.filterByLot.setFilter(e.detail.value)
+              }
+            >
               <IonItem>
-                <IonLabel>View old events</IonLabel>
-                <IonToggle
-                  value="expired"
-                  checked={isExpired}
-                  onIonChange={(e) => {
-                    setIsExpired(e.detail.checked);
-                    props.filterByExpired &&
-                      props.filterByExpired(e.detail.checked);
-                  }}
-                />
+                <IonLabel>Student</IonLabel>
+                <IonRadio value="Student" />
               </IonItem>
-            )}
-          </IonList>
-        </IonContent>
-      </IonMenu>
-    </>
-  );
-};
+              <IonItem>
+                <IonLabel>Employee</IonLabel>
+                <IonRadio value="Employee" />
+              </IonItem>
+            </IonRadioGroup>
+          )}
+          {props.filterByExpired && (
+            <IonItem>
+              <IonLabel>View old events</IonLabel>
+              <IonToggle
+                value="expired"
+                checked={props.filterByExpired.filter}
+                onIonChange={(e) => {
+                  props.filterByExpired &&
+                    props.filterByExpired.setFilter(e.detail.checked);
+                }}
+              />
+            </IonItem>
+          )}
+        </IonList>
+      </IonContent>
+    </IonMenu>
+  </>
+);
